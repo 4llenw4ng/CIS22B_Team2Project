1d9c09772c23672317a5433e035b56666684a5c5
4llenw4ng <33169511+4llenw4ng@users.noreply.github.com>
2018-11-20 15:17:24 -0800

    Finalized version of Money

--- Serendipity Booksellers Software/Money.cpp
+++ Serendipity Booksellers Software/Money.cpp
@@ -34,5 +34,25 @@ void Money::setCents(int i)
 
 void Money::addMoney(Money m)
 {
+	this->dollars += m.getDollars();
+	this->cents += m.getCents();
+	this->trim();
+}
 
+void Money::trim()
+{
+	while (cents >= 100)
+	{
+		dollars++;
+		cents -= 100;
+	}
+}
+
+Money operator+(const Money &m1, const Money &m2)
+{
+	Money m;
+	m.setDollars(m1.dollars + m2.dollars);
+	m.setCents(m1.cents + m2.cents);
+	m.trim();
+	return m;
 }
