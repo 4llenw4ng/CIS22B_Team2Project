d5deab03b6f99564bbb4d1e8db21f8b3d4b8fd8e
4llenw4ng <33169511+4llenw4ng@users.noreply.github.com>
2018-11-20 15:18:27 -0800

    Finalized header

--- Serendipity Booksellers Software/Money.h
+++ Serendipity Booksellers Software/Money.h
@@ -1,3 +1,5 @@
+//To simulate an amount of money
+#pragma once
 #include <iostream>
 
 class Money
@@ -12,6 +14,8 @@ public:
 	int getCents();
 	void setDollars(int i);
 	void setCents(int i);
-	void addMoney(Money m);
-	void subtractMoney(Money m);
+	void addMoney(Money m); //adds m to this, trimming accordingly
+	void trim(); //trims the amount (adequately tweaks any value of >= 100 in cents to an appropriate value)
+	//void subtractMoney(Money m);
+	friend Money operator+(const Money &m1, const Money &m2);//OPERATOR OVERLOADING, returns the trimmed sum of m1 and m2 
 };
